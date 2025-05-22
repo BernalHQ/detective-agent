b3c99179105af0e7d67c7f0ad2a0bec1c66454a8
BernalHQ <bernalhq19@gmail.com>
2025-05-21 19:05:29 -0600

    add package dotenv

--- server/utils/etherscan.js
+++ server/utils/etherscan.js
@@ -1,6 +1,40 @@
 
-function getAddressInfo() {
-    console.log('Hello mother fuyckers');
+import dotenv from 'dotenv';
+dotenv.config();
+const API_KEY = '5TKVKGRMNPSC4P1ITI8NMUSJGSE2A2TUAY';
+const address = '0xc5102fE9359FD9a28f877a67E36B0F050d81a3CC';
+
+const url = `https://api.etherscan.io/api?module=account&action=txlist&address=${address}&startblock=0&endblock=99999999&page=1&offset=10&sort=asc&apikey=${API_KEY}`;
+
+/*
+fetch(url)
+  .then(res => res.json())
+  .then(data => {
+    console.log(JSON.stringify(data, null, 2));
+  })
+  .catch(err => {
+    console.error('Error:', err);
+  });
+*/
+
+
+const urlApi = `${process.env.ETHERSCAN_API_URL}
+   ?chainid=1
+   &module=account
+   &action=txlist
+   &address=0xc5102fE9359FD9a28f877a67E36B0F050d81a3CC
+   &startblock=0
+   &endblock=99999999
+   &page=1
+   &offset=10
+   &sort=asc
+   &apikey=${process.env.DETECTIVE_AGENT_KEY}`;
+
+async function getAddressInfo() {
+    const response = await fetch(url);
+    const addressInfo = await response.json();
+    
+    console.log(addressInfo);
 }
 
-getAddressInfo();
+getAddressInfo(); 
